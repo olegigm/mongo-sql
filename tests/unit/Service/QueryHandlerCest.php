d109d82ad920f86003feb4e829718c0ddfda8aee
Oleg Mishchenko <olegigm@gmail.com>
2017-03-20 13:45:20 +0200

    added selectQueryExecute test

--- tests/unit/Service/QueryHandlerCest.php
+++ tests/unit/Service/QueryHandlerCest.php
@@ -47,11 +47,83 @@ class QueryHandlerCest
     public function testUseDB(UnitTester $I)
     {
         $I->wantToTest('select database');
+
         $databaseName = $I->getConfig('database_name');
-        $sql = 'use ' . $databaseName;
-        $result = $this->queryHandler->handle($sql);
+        $result = $this->useDatabase($databaseName);
+
         $I->assertTrue($result instanceof ProcessorResult);
         $I->assertEquals($result->getType(), ProcessorResult::TYPE_STRING);
         $I->assertEquals($result->getStrData(), sprintf('Switched to db %s', $databaseName));
     }
+
+
+    public function selectQueryExecuteTest(UnitTester $I)
+    {
+        $I->wantToTest('query data');
+
+        $this->useDatabase($I->getConfig('database_name'));
+
+        $sql = 'select author, title, position from books where position > 1 and position < 5';
+        $result = $this->queryHandler->handle($sql);
+
+        $I->assertTrue($result instanceof ProcessorResult);
+        $I->assertEquals($result->getType(), ProcessorResult::TYPE_TABLE);
+        $I->assertEquals($this->checkSelectResultArray(), $this->resultToArray($result->getData()));
+    }
+
+    /**
+     * @param string $databaseName
+     * @return ProcessorResult|string
+     */
+    private function useDatabase($databaseName)
+    {
+        $sql = 'use ' . $databaseName;
+        return $this->queryHandler->handle($sql);
+    }
+
+    private function checkSelectResultArray()
+    {
+        return [
+                [
+                    0 => 'Brit Bennett',
+                    1 => 'The Mothers',
+                    2 => 2,
+                ],
+                [
+                    0 => 'Adam Haslett',
+                    1 => 'Imagine Me Gone',
+                    2 => 3,
+                ],
+                [
+                    0 => 'Yaa Gyasi',
+                    1 => 'Homegoing',
+                    2 => 4,
+                ],
+        ];
+    }
+
+    private function resultToArray(array $data) : array
+    {
+        $data = json_decode(json_encode($data), true);
+        $headers = array_keys($data[0]);
+
+        $rows = [];
+        foreach ($data as $key => $row) {
+            foreach ($headers as $column) {
+                if (array_key_exists($column, $row)) {
+                    if ($column == '_id') {
+                        $rows[$key][] = $row[$column]['$oid'];
+                    } else {
+                        $rows[$key][] = (is_array($row[$column])) ? json_encode($row[$column]) : $row[$column];
+                    }
+                } else {
+                    $rows[$key][] = '';
+                }
+            }
+        }
+
+        return $rows;
+    }
+
+
 }
